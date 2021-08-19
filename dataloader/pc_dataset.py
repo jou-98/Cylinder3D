6811bf4ceab4d919fcc38703b6647221d9f5afd7
Yingchen Nie <yingchennie@gmail.com>
2021-08-19 20:30:32 +1000

    adapt to bolt dataset

--- dataloader/pc_dataset.py
+++ dataloader/pc_dataset.py
@@ -344,6 +344,48 @@ class SemKITTI_sk_multiscan(data.Dataset):
         return data_tuple
 
 
+@register_dataset
+class Rockbolts_sk(data.Dataset):
+    def __init__(self, data_path, imageset='train',
+                 return_ref=False, label_mapping="bolts.yaml", nusc=None):
+        self.return_ref = return_ref
+        with open(label_mapping, 'r') as stream:
+            boltyaml = yaml.safe_load(stream)
+        self.learning_map = boltyaml['learning_map']
+        self.imageset = imageset
+        if imageset == 'train':
+            split = boltyaml['split']['train']
+        elif imageset == 'val':
+            split = boltyaml['split']['valid']
+        elif imageset == 'test':
+            split = boltyaml['split']['test']
+        else:
+            raise Exception('Split must be train/val/test')
+
+        self.im_idx = []
+        for i_folder in split:
+            self.im_idx += absoluteFilePaths('/'.join([data_path, str(i_folder).zfill(2), 'velodyne']))
+
+    def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.im_idx)
+
+    def __getitem__(self, index):
+        raw_data = np.fromfile(self.im_idx[index], dtype=np.float16).reshape((-1, 3)) # Changed from (-1,4)
+        if self.imageset == 'test':
+            annotated_data = np.expand_dims(np.zeros_like(raw_data[:, 0], dtype=int), axis=1)
+        else:
+            annotated_data = np.fromfile(self.im_idx[index].replace('velodyne', 'labels')[:-3] + 'label',
+                                         dtype=np.ubyte).reshape((-1, 1))
+            annotated_data = annotated_data # & 0xFFFF  # delete high 16 digits binary
+            annotated_data = np.vectorize(self.learning_map.__getitem__)(annotated_data)
+
+        data_tuple = (raw_data[:, :3], annotated_data.astype(np.uint8))
+        if self.return_ref:
+            data_tuple += (raw_data[:, 3],)
+        return data_tuple
+
+
 # load Semantic KITTI class info
 
 def get_SemKITTI_label_name(label_mapping):
